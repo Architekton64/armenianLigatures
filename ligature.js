0978ed18a05bc303ed384b6e194f2429710181b2
Architekton64 <you@example.com>
2018-04-05 03:59:55 +0400

    initial commit

---
+++ ligature.js
@@ -0,0 +1,8 @@
+ligature = () => {
+         document.body.innerHTML = document.body.innerHTML
+		.replace(/մխ/g, 'ﬗ')
+		.replace(/մն/g, 'ﬓ')
+		.replace(/մե/g, 'ﬔ')
+		.replace(/մի/g, 'ﬕ')
+		.replace(/վն/g, 'ﬖ');
+};
